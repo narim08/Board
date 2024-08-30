1a61cb2c538e7502e968cea90892e388289c3dc0
Narim Park <skfla2459@gmail.com>
2024-08-30 16:24:33 +0900

    first commit

---
+++ src/main/java/com/study/board/entity/Board.java
@@ -0,0 +1,21 @@
+package com.study.board.entity;
+
+import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
+@Data
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+
+    private String content;
+}
